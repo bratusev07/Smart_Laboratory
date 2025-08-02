8290ce6fd203f1e641440ad7f974d65299bd13e4
kiotel <valera.lihenko@gmail.com>
2025-08-02 22:42:18 +0300

    Чуток верстки

--- ui/core/src/commonMain/kotlin/ru/bratusev/smartlab/ui/core/components/TileVerticalGrid.kt
+++ ui/core/src/commonMain/kotlin/ru/bratusev/smartlab/ui/core/components/TileVerticalGrid.kt
@@ -1,22 +1,24 @@
 package ru.bratusev.smartlab.ui.core.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import ru.bratusev.smartlab.ui.core.models.TileButtonUi
 import ru.bratusev.smartlab.ui.core.theme.AppTheme
@@ -26,24 +28,42 @@ fun TileVerticalGrid(
     modifier: Modifier = Modifier,
     title: String,
     data: List<TileButtonUi>,
+    columnsAmount: Int = 2,
 ) {
-    Box(modifier = modifier.background(MaterialTheme.colorScheme.surface)) {
-        Text(
-            modifier = Modifier.wrapContentWidth().zIndex(1f).padding(start = 10.dp),
-            text = title,
-            style = MaterialTheme.typography.titleMedium
-        )
-        LazyVerticalGrid(
-            contentPadding = PaddingValues(top = 20.dp),
-            columns = GridCells.Fixed(2),
-            horizontalArrangement = Arrangement.Center,
-            content = {
-                items(data) {
-                    TileButton(
-                        modifier = Modifier.padding(10.dp), tileButtonUi = it, onClick = {})
+    var isReversed by rememberSaveable { mutableStateOf(false) }
+
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(columnsAmount),
+        horizontalArrangement = Arrangement.Center,
+        content = {
+            stickyHeader {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Button(
+                        onClick = { isReversed = !isReversed }) {
+                        Text("TODO")
+                    }
                 }
-            })
-    }
+            }
+            items(data.reversedIf(isReversed)) {
+                TileButton(
+                    modifier = Modifier.padding(10.dp), tileButtonUi = it, onClick = {})
+            }
+        })
+}
+
+//
+private fun <E> List<E>.reversedIf(flag: Boolean): List<E> {
+    return if (flag) this.reversed()
+    else this
 }
 
 @Preview(
@@ -64,8 +84,7 @@ private fun TileVerticalGridPreviewLightBulbs() {
     }
     AppTheme {
         TileVerticalGrid(
-            title = "Light bulbs",
-            data = mockData
+            title = "Light bulbs", data = mockData
         )
     }
 }
@@ -88,8 +107,7 @@ private fun TileVerticalGridPreviewThermometers() {
     }
     AppTheme(darkTheme = true) {
         TileVerticalGrid(
-            title = "Thermometers",
-            data = mockData
+            title = "Thermometers", data = mockData
         )
     }
 }
@@ -100,7 +118,6 @@ private fun TileVerticalGridPreviewThermometers() {
 @Composable
 fun ManyGridsPreview() {
     // Это то, как я на данный момент вижу верстку этого меню
-
     val mockDataThermometers = buildList {
         for (i in 1..30) {
             add(
@@ -125,34 +142,39 @@ fun ManyGridsPreview() {
     }
 
     AppTheme {
-        LazyRow {
-            item {
-                TileVerticalGrid(
-                    modifier = Modifier.width(300.dp),
-                    title = "LightBulbs1",
-                    data = mockDataLightBulbs,
-                )
-            }
-            item {
-                TileVerticalGrid(
-                    modifier = Modifier.width(300.dp),
-                    title = "Thermometers1",
-                    data = mockDataThermometers
-                )
-            }
-            item {
-                TileVerticalGrid(
-                    modifier = Modifier.width(300.dp),
-                    title = "LightBulbs2",
-                    data = mockDataLightBulbs
-                )
-            }
-            item {
-                TileVerticalGrid(
-                    modifier = Modifier.width(300.dp),
-                    title = "Thermometers2",
-                    data = mockDataThermometers
-                )
+        Row {
+            Text(modifier = Modifier.width(100.dp), text = "Боковое менб")
+            LazyRow(
+                modifier = Modifier.weight(1f), horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                item {
+                    TileVerticalGrid(
+                        modifier = Modifier.fillParentMaxWidth(),
+                        title = "LightBulbs1",
+                        data = mockDataLightBulbs,
+                    )
+                }
+                item {
+                    TileVerticalGrid(
+                        modifier = Modifier.fillParentMaxWidth(),
+                        title = "Thermometers1",
+                        data = mockDataThermometers
+                    )
+                }
+                item {
+                    TileVerticalGrid(
+                        modifier = Modifier.fillParentMaxWidth(),
+                        title = "LightBulbs2",
+                        data = mockDataLightBulbs
+                    )
+                }
+                item {
+                    TileVerticalGrid(
+                        modifier = Modifier.fillParentMaxWidth(),
+                        title = "Thermometers2",
+                        data = mockDataThermometers
+                    )
+                }
             }
         }
     }
