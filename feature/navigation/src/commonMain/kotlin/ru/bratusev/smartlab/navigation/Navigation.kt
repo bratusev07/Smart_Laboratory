88d807a059577613088650666d63da1ff267037c
kiotel <valera.lihenko@gmail.com>
2025-08-12 17:46:19 +0300

    Добавил drawer

--- feature/navigation/src/commonMain/kotlin/ru/bratusev/smartlab/navigation/Navigation.kt
+++ feature/navigation/src/commonMain/kotlin/ru/bratusev/smartlab/navigation/Navigation.kt
@@ -1,11 +1,22 @@
 package ru.bratusev.smartlab.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import kotlinx.coroutines.launch
 import ru.bratusev.smartlab.feature_home.HomeScreen
+import ru.bratusev.smartlab.feature_home.NavigationDrawer
 import ru.bratusev.smartlab.feature_login.LoginScreen
 import ru.bratusev.smartlab.feature_settings.SettingsScreen
 
@@ -15,16 +26,35 @@ sealed class Screen(val route: String) {
     object Settings : Screen("settings")
 }
 
+// TODO: кол-во фич = кол-во багов
 @Composable
 fun AppNavigation(navController: NavHostController) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val drawerScope = rememberCoroutineScope()
+    var isDrawerHidden by rememberSaveable { mutableStateOf(true) }
+
+    NavigationDrawer(
+        scope = drawerScope,
+        drawerState = drawerState,
+        isHidden = isDrawerHidden,
+        navigateToAuthScreen = {
+            navController.navigate(Screen.Login.route)
+            isDrawerHidden = true
+        }) { paddingValues ->
         NavHost(
-            navController = navController, startDestination = Screen.Login.route
+            modifier = Modifier.padding(paddingValues),
+            navController = navController,
+            startDestination = Screen.Login.route
         ) {
 
             composable(Screen.Login.route) {
                 LoginScreen(
                     navigateToHome = {
-                        navController.navigate(Screen.Home.route)
+                        drawerScope.launch {
+                            drawerState.close()
+                            isDrawerHidden = false
+                            navController.navigate(Screen.Home.route)
+                        }
                     })
             }
 
@@ -42,7 +72,10 @@ fun AppNavigation(navController: NavHostController) {
                     })
             }
         }
+    }
 }
+
+fun mutableStateOf(bool: Boolean) {}
 
 private fun NavController.navigate(route: String?) {
     if (route == null) {
