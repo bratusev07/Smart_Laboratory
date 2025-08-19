b9b84560d616f77c0139f529f3c07c5eb9e38229
Денис Братусев <55459712+bratusev07@users.noreply.github.com>
2025-08-19 12:57:12 +0300

    Добавил отображение сенсоров в виде сетки

--- feature/home/src/commonMain/kotlin/ru/bratusev/smartlab/feature_home/mappers/HomeUiMapper.kt
+++ feature/home/src/commonMain/kotlin/ru/bratusev/smartlab/feature_home/mappers/HomeUiMapper.kt
@@ -1,2 +1,50 @@
 package ru.bratusev.smartlab.feature_home.mappers
 
+import ru.bratusev.smartlab.domain.core.model.socket.ServiceEntity
+import ru.bratusev.smartlab.ui.core.models.sensorCard.SensorCardRes
+import ru.bratusev.smartlab.ui.core.models.sensorCard.SensorCardState
+import ru.bratusev.smartlab.ui.core.models.sensorCard.SensorCardTints
+import ru.bratusev.smartlab.ui.core.models.sensorCard.SensorCardUi
+import ru.bratusev.smartlab.ui.core.models.sensorCard.SensorCardVerticalGridUi
+import ru.bratusev.smartlab.ui.core.theme.SensorCardCommonColors
+
+fun List<ServiceEntity>.mapToServiceListUi() = SensorCardVerticalGridUi(
+    sensors = this.map { it.mapToUi() },
+    columnsAmount = 2
+)
+
+private fun ServiceEntity.mapToUi(): SensorCardUi = when (domain?.lowercase()) {
+    "switch" -> this.mapSwitchToUi()
+    else -> this.mapDefaultToUi()
+}
+
+private fun ServiceEntity.mapDefaultToUi() = SensorCardUi.Small(
+    id = id.orEmpty(),
+    state = state.mapToSensorState(),
+    domain = domain.orEmpty(),
+    drawableResource = SensorCardRes.lightBulb,
+    tints = SensorCardTints(
+        SensorCardCommonColors.LightBulb.On,
+        SensorCardCommonColors.LightBulb.Off,
+        SensorCardCommonColors.LightBulb.Unavailable
+    ),
+)
+
+private fun ServiceEntity.mapSwitchToUi() = SensorCardUi.Medium(
+    title = id.orEmpty(),
+    id = id.orEmpty(),
+    state = state.mapToSensorState(),
+    domain = domain.orEmpty(),
+    drawableResource = SensorCardRes.lightBulb,
+    tints = SensorCardTints(
+        SensorCardCommonColors.LightBulb.On,
+        SensorCardCommonColors.LightBulb.Off,
+        SensorCardCommonColors.LightBulb.Unavailable
+    ),
+)
+
+private fun String?.mapToSensorState() = when (this?.lowercase()) {
+    "on", "\"on\"" -> SensorCardState.On
+    "off", "\"off\"" -> SensorCardState.Off
+    else -> SensorCardState.Unavailable
+}
