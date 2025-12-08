1e189e3e21654e403985424274fdc884554c65cb
Денис Братусев <55459712+bratusev07@users.noreply.github.com>
2025-12-08 14:39:47 +0300

    automation ui update

--- feature/automation/src/commonMain/kotlin/ru/bratusev/smartlab/feature_automation/AutomationScreen.kt
+++ feature/automation/src/commonMain/kotlin/ru/bratusev/smartlab/feature_automation/AutomationScreen.kt
@@ -1,9 +1,17 @@
 package ru.bratusev.smartlab.feature_automation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
 import ru.bratusev.smartlab.feature_automation.models.Event
 import ru.bratusev.smartlab.navigation.api.NavigationApi
@@ -58,10 +67,21 @@ fun AutomationScreen(
             isSheetOpen = true
         }
 
-        FloatingActionButton(
-            modifier = Modifier.align(Alignment.BottomEnd),
-            onClick = {vm.handleEvent(Event.OnSaveAutomation)}
-        ) {}
+        Row(modifier = Modifier.align(Alignment.BottomEnd).padding(bottom = 10.dp, end = 5.dp)) {
+            FloatingActionButton(
+                onClick = {vm.handleEvent(Event.OnSaveAutomation)}
+            ) {
+                Icon(Icons.Default.Save, contentDescription = "Save")
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            FloatingActionButton(
+                onClick = {}
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
     }
 }
 
